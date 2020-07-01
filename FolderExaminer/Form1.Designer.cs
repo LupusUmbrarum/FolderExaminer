bc97c0e262810459ecfd59411dd982190a150daa
N_Cowley <cowley.nate.m9@gmail.com>
2020-07-01 11:59:44 -0500

    Initial Commit
    Contains all of the previous work.

---
+++ FolderExaminer/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace FolderExaminer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.findFolderButton = new System.Windows.Forms.Button();
+			this.folderPathTextBox = new System.Windows.Forms.TextBox();
+			this.examineFolderButton = new System.Windows.Forms.Button();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+			this.SuspendLayout();
+			// 
+			// findFolderButton
+			// 
+			this.findFolderButton.Location = new System.Drawing.Point(112, 13);
+			this.findFolderButton.Name = "findFolderButton";
+			this.findFolderButton.Size = new System.Drawing.Size(75, 23);
+			this.findFolderButton.TabIndex = 0;
+			this.findFolderButton.Text = "Find Folder";
+			this.findFolderButton.UseVisualStyleBackColor = true;
+			this.findFolderButton.Click += new System.EventHandler(this.findFolderButton_Click);
+			// 
+			// folderPathTextBox
+			// 
+			this.folderPathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.folderPathTextBox.Location = new System.Drawing.Point(193, 15);
+			this.folderPathTextBox.Name = "folderPathTextBox";
+			this.folderPathTextBox.Size = new System.Drawing.Size(595, 20);
+			this.folderPathTextBox.TabIndex = 1;
+			this.folderPathTextBox.DoubleClick += new System.EventHandler(this.folderPathTextBox_DoubleClick);
+			// 
+			// examineFolderButton
+			// 
+			this.examineFolderButton.Location = new System.Drawing.Point(12, 13);
+			this.examineFolderButton.Name = "examineFolderButton";
+			this.examineFolderButton.Size = new System.Drawing.Size(94, 23);
+			this.examineFolderButton.TabIndex = 0;
+			this.examineFolderButton.Text = "Examine Folder";
+			this.examineFolderButton.UseVisualStyleBackColor = true;
+			this.examineFolderButton.Click += new System.EventHandler(this.examineFolderButton_Click);
+			// 
+			// treeView1
+			// 
+			this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.treeView1.Location = new System.Drawing.Point(12, 42);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(776, 396);
+			this.treeView1.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.treeView1);
+			this.Controls.Add(this.folderPathTextBox);
+			this.Controls.Add(this.examineFolderButton);
+			this.Controls.Add(this.findFolderButton);
+			this.Name = "Form1";
+			this.Text = "FolderExaminer";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button findFolderButton;
+		private System.Windows.Forms.TextBox folderPathTextBox;
+		private System.Windows.Forms.Button examineFolderButton;
+		private System.Windows.Forms.TreeView treeView1;
+		private System.Windows.Forms.FolderBrowserDialog fbd;
+	}
+}
+
---
+++ FolderExaminer/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FolderExaminer
+{
+	public partial class Form1 : Form
+	{
+		private string currentPath = "";
+		private string folderToCheck = "";
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void findFolderButton_Click(object sender, EventArgs e)
+		{
+			findFolder();
+		}
+
+		private void folderPathTextBox_DoubleClick(object sender, EventArgs e)
+		{
+			findFolder();
+		}
+
+		private void examineFolderButton_Click(object sender, EventArgs e)
+		{
+			currentPath = folderPathTextBox.Text;
+			folderToCheck = currentPath.Substring(currentPath.LastIndexOf('\\') + 1);
+
+			if(currentPath == "" || !isFolder(currentPath))
+			{
+				return;
+			}
+
+			examineFolder();
+		}
+
+		private void findFolder()
+		{
+			if(fbd.ShowDialog() == DialogResult.OK || fbd.ShowDialog() == DialogResult.Yes)
+			{
+				folderPathTextBox.Text = fbd.SelectedPath;
+			}
+		}
+
+		private bool isFolder(string path)
+		{
+			return Directory.Exists(path);
+		}
+
+		private Folder getFolders(string path)
+		{
+			Folder folder = new Folder(path.Substring(path.LastIndexOf('\\') + 1));
+
+			try
+			{
+				foreach(string s in Directory.GetFiles(path))
+				{
+					folder.files.Add(s);
+				}
+
+				foreach(string s in Directory.GetDirectories(path))
+				{
+					folder.folders.Add(getFolders(path + "\\" + s));
+				}
+			}
+			catch(Exception e)
+			{
+				MessageBox.Show(e.Message);
+			}
+
+			return folder;
+		}
+
+		private Folder getItemsInFolder(string path)
+		{
+			List<string> items = new List<string>();
+			Folder rootFolder = new Folder(folderToCheck);
+
+			try
+			{
+				// get the files in the directory
+				foreach(string s in Directory.GetFiles(path))
+				{
+					items.Add(s.Substring(s.IndexOf(folderToCheck) + folderToCheck.Length + 1));
+					rootFolder.files.Add(s.Substring(s.IndexOf(folderToCheck) + folderToCheck.Length + 1));
+				}
+
+				// get the directories in the directory
+				foreach(string s in Directory.GetDirectories(path))
+				{
+					rootFolder.folders.Add(getItemsInFolder(s));
+				}
+			}
+			catch(Exception e)
+			{
+				MessageBox.Show(e.Message);
+			}
+
+			return rootFolder;
+		}
+
+		private List<Folder> createFolderStructure(string path)
+		{
+			List<Folder> folders = new List<Folder>();
+			/*
+			 * backup of getItemsInFolder
+			try
+			{
+				// get the files in the directory
+				foreach (string s in Directory.GetFiles(path))
+				{
+					items.Add(s.Substring(s.IndexOf(folderToCheck) + folderToCheck.Length + 1));
+					rootFolder.files.Add(s.Substring(s.IndexOf(folderToCheck) + folderToCheck.Length + 1));
+				}
+
+				// get the directories in the directory
+				foreach (string s in Directory.GetDirectories(path))
+				{
+					items.AddRange(getItemsInFolder(s));
+					rootFolder.folders.AddRange(getItemsInFolder(s));
+				}
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show(e.Message);
+			}*/
+
+			return null;
+		}
+
+		private void examineFolderOld()
+		{
+			//List<string> items = getItemsInFolder(currentPath);
+			//List<Folder> folders = createFolderStructure(getItemsInFolder(currentPath));
+			Folder f = getItemsInFolder(currentPath);
+
+			if(items.Count > 0)
+			{
+				treeView1.BeginUpdate();
+				treeView1.Nodes.Clear();
+			}
+			else
+			{
+				return;
+			}
+
+			TreeNode rootDirectory = new TreeNode(folderToCheck);
+			treeView1.Nodes.Add(rootDirectory);
+
+			for(int i = 0; i < items.Count; i++)
+			{
+				TreeNode newNode = new TreeNode(items[i]);
+
+				rootDirectory.Nodes.Add(newNode);
+			}
+
+			treeView1.EndUpdate();
+
+			treeView1.Nodes[0].Expand();
+		}
+
+		private TreeNode createTreeFromFolders(string path)
+		{
+			TreeNode rootNode = new TreeNode(folderToCheck);
+
+			Folder rootFolder = getItemsInFolder(currentPath);
+
+			Folder currentFolder = rootFolder;
+
+			while(currentFolder != null)
+			{
+				foreach(Folder f in currentFolder.folders)
+				{
+
+				}
+			}
+		}
+	}
+
+	class Folder
+	{
+		public string title;
+
+		public List<Folder> folders = new List<Folder>();
+		public List<string> files = new List<string>();
+
+		public Folder() : this("")
+		{
+
+		}
+
+		public Folder(string title = "")
+		{
+			this.title = title;
+		}
+	}
+}
